116e0392aad89e8e3ad66c0a8a00b1b111538ff1
gg3104634-ship-it <gg3104634@gmail.com>
2026-06-10 08:45:05 +0530

    Add files via upload

---
+++ sum of array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int arr[2]={10,20};
+
+    int max = myarray[0];
+    for(int i=0; i<2; i++){
+        if(myarray[i]> max) 
+        max= myarray[i];
+    }
+    printf("max%d",max);
+   
+return 0;
+}
+
+
+
+
